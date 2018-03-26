bb63d7ecc1278f4427380f8d0b37c9526f1c1da2
CDC Team <cdc_dev@CDC.local>
2018-03-26 22:37:55 +0400

    added feed page

---
+++ lib/Feed.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class Feed extends StatelessWidget{
+
+ @override
+ Widget build (BuildContext context){
+
+    return new Container(
+
+      child:new Center(
+
+        child: new Icon(Icons.collections,size: 150.0,color:Colors.brown),
+      ),
+
+    );
+
+ }
+}
