cd1fa9dd6b4bca87fb6f700a97c8780798768211
CDC Team <cdc_dev@CDC.local>
2018-03-26 21:58:48 +0400

    test cases added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:eduworld/main.dart';
@@ -15,15 +15,9 @@ void main() {
     await tester.pumpWidget(new MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+   
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+   
   });
 }
